018ab3a8874da2c1675a7356633ef5a84997e0cd
aziz_maaref <davidpunisher475@gmail.com>
2024-07-13 10:11:26 +0100

    Search Bar In Product Page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
   <div className="centred max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 flex flex-col justify-center">
     <div className="lg:flex lg:items-start lg:gap-8">
       <div className="text-white">
-       <img src="/img/logo.png" alt="logo" className='bg-white rounded-xl mt-3' />
+       <img src="/img/logo.png" alt="logo" className='bg-white rounded-xl mt-3 ' />
       </div>
 
       <div className="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16">
