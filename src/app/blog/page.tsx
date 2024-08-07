a06541579649631090a2e6caf04e7de1a9c48294
aziz_maaref <davidpunisher475@gmail.com>
2024-08-07 14:04:55 +0100

    Add Blog With PostMan to MongoDB  Version 2

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -28,10 +28,10 @@ const Page: React.FC = async () => {
       <div className='grid gap-2 sm:gap-2 md:gap-6 lg:gap-6 xl:gap-6 2xl:gap-6 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3'>
         {blogs.map((blog) => (
           <div 
-            key={blog.id} // Use _id instead of id for MongoDB
+            key={blog._id} // Use _id instead of id for MongoDB
             className={`w-full mb-2 overflow-hidden block rounded-xl border shadow-xl transition hover:border-orange-400/10 hover:shadow-orange-400/10 flex-1`}
           >
-            <Link href={`/blog_details/${blog.id}`} className="relative block">
+            <Link href={`/blog_details/${blog._id}`} className="relative block">
               <Image 
                 src={blog.imgSrc} 
                 width={500} height={100}
@@ -40,21 +40,21 @@ const Page: React.FC = async () => {
               />
             </Link>
             <div className="flex flex-col gap-1 bg-white rounded-xl p-4 h-42 -mt-4">
-              <Link href={`/blog_details/${blog.id}`} className="text-[10px] sm:text-[10px] md:text-[15px] lg:text-[15px] xl:text-[15px] 2xl:text-[15px] text-orange-300">{blog.Categorie}</Link>
+              <Link href={`/blog_details/${blog._id}`} className="text-[10px] sm:text-[10px] md:text-[15px] lg:text-[15px] xl:text-[15px] 2xl:text-[15px] text-orange-300">{blog.Categorie}</Link>
               <div className="flex items">
-                <Link href={`/blog_details/${blog.id}`} className="text-sm sm:text-sm md:text-xl font-semibold text-gray-800 drop-shadow-xl truncate">{blog.title}</Link>
+                <Link href={`/blog_details/${blog._id}`} className="text-sm sm:text-sm md:text-xl font-semibold text-gray-800 drop-shadow-xl truncate">{blog.title}</Link>
                 <FaArrowRight className="text-black ml-3 -rotate-45" />
               </div>
               <p className="text-xs sm:text-sm text-gray-400 mb-0 sm:mb-0 md:mb-2 lg:mb-2 xl:mb-2 2xl:mb-2 line-clamp-2">{blog.description}</p>
               <div className="flex items-center gap-2 text-xs sm:text-sm -mx-2 sm:-mx-2 md:-mx-0 lg:-mx-0 xl:-mx-0 2xl:-mx-0">
                 <div className="flex items-center gap-1">
                   <AiOutlineHeart className="text-[15px] sm:text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[20px]"/>
-                  <Link href={`/blog_details/${blog.id}`} className="text-gray-800 text-[10px] sm:text-[10px] md:text-[15px] lg:text-[15px] xl:text-[15px] 2xl:text-[15px]">{blog.likes}</Link>
+                  <Link href={`/blog_details/${blog._id}`} className="text-gray-800 text-[10px] sm:text-[10px] md:text-[15px] lg:text-[15px] xl:text-[15px] 2xl:text-[15px]">{blog.likes}</Link>
                   <p className="text-gray-800 text-[10px] sm:text-[10px] md:text-[15px] lg:text-[15px] xl:text-[15px] 2xl:text-[15px]">likes</p>
                 </div>
                 <div className="flex items-center gap-1">
                   <AiOutlineComment className="text-[15px] sm:text-[15px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[20px]" />
-                  <Link href={`/blog_details/${blog.id}`} className="text-gray-800 text-[10px] sm:text-[10px] md:text-[15px] lg:text-[15px] xl:text-[15px] 2xl:text-[15px]">{blog.comments}</Link>
+                  <Link href={`/blog_details/${blog._id}`} className="text-gray-800 text-[10px] sm:text-[10px] md:text-[15px] lg:text-[15px] xl:text-[15px] 2xl:text-[15px]">{blog.comments}</Link>
                   <p className="text-gray-800 text-[10px] sm:text-[10px] md:text-[15px] lg:text-[15px] xl:text-[15px] 2xl:text-[15px]">comments</p>
                 </div>
               </div>
